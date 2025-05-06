8d07eb35159490f80f0864a4f1234d8cc4c36577
development0261 <development0261@gmail.com>
2025-05-06 07:14:24 +0000

    route changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,48 +1,49 @@
-import { Routes } from "@angular/router";
+import { Route, Routes } from "@angular/router";
 import { AccessGuard } from "./access.guard";
-import { AccessService } from "./access.service";
 import { inject } from "@angular/core";
 
+const accessGuardFn = (route: Route, segments: import("@angular/router").UrlSegment[]) => {
+    return inject(AccessGuard).canActivate(route as any, { url: '/' });
+}
+
 export const routes: Routes = [
-  {
-    path: "reports",
-      loadChildren: () =>
-          import("./modules/reports/reports").then((m) => m.routes),
-    canMatch: [AccessGuard],
-    data: { moduleName: "Reports" }
-  },
-  {
-    path: "files",
-      loadChildren: () =>
-          import("./modules/files/files").then((m) => m.routes),
-    canMatch: [AccessGuard],
-    data: { moduleName: "Files" }
-  },
-  {
-    path: "users",
-      loadChildren: () =>
-          import("./modules/users/users").then((m) => m.routes),
-    canMatch: [AccessGuard],
-    data: { moduleName: "Users" }
-  },
-  {
-    path: "projects",
-      loadChildren: () =>
-          import("./modules/projects/projects").then((m) => m.routes),
-    canMatch: [AccessGuard],
-    data: { moduleName: "Projects" }
-  },
     {
-    path: "settings",
-    loadChildren: () =>
-      import("./modules/settings/settings").then((m) => m.routes),
-      canMatch: [AccessGuard],
-    data: { moduleName: "Settings" }
-  },
-  {
-    path: "forbidden",
-    loadComponent: () =>
-      import("./forbidden/forbidden.component").then((m) => m.ForbiddenComponent),
-  },
+        path: "reports",
+        loadChildren: () =>
+            import("./modules/reports/reports").then((m) => m.routes),
+        canMatch: [accessGuardFn],
+        data: { moduleName: "Reports" }
+    },
+    {
+        path: "files",
+        loadChildren: () =>
+            import("./modules/files/files").then((m) => m.routes),
+        canMatch: [accessGuardFn],
+        data: { moduleName: "Files" }
+    },
+    {
+        path: "users",
+        loadChildren: () =>
+            import("./modules/users/users").then((m) => m.routes),
+        canMatch: [accessGuardFn],
+        data: { moduleName: "Users" }
+    },
+    {
+        path: "projects",
+        loadChildren: () =>
+            import("./modules/projects/projects").then((m) => m.routes),
+        canMatch: [accessGuardFn],
+        data: { moduleName: "Projects" }
+    },
+    {
+        path: "settings",
+        loadChildren: () => import("./modules/settings/settings").then((m) => m.routes),
+        canMatch: [accessGuardFn],
+        data: { moduleName: "Settings" }
+    },
+    {
+        path: "forbidden",
+        loadComponent: () => import("./forbidden/forbidden.component").then((m) => m.ForbiddenComponent),
+    },
 
 ];
